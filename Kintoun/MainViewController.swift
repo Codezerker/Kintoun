271f5f1a5b8607155ec970772bce1cd202c080b2
Jesse <jesse0628@gmail.com>
2016-04-22 23:05:37 +1200

    adding simple test download UI

--- Kintoun/MainViewController.swift
+++ Kintoun/MainViewController.swift
@@ -10,13 +10,22 @@ import Cocoa
 
 class MainViewController: NSViewController {
 
+    @IBOutlet weak var urlsTextField: NSTextField!
+    @IBOutlet weak var savePathLabel: NSTextField!
+    
+    private var downloadFolderPath = NSURL.fileURLWithPath(NSHomeDirectory() + "/Downloads/")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.savePathLabel.stringValue = downloadFolderPath.path!
+        
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(didConnectNotification), name: AriaClientNotificationKey.Connected.rawValue, object: nil)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(globalStatChanged), name: AriaClientNotificationKey.GlobalStatChanged.rawValue, object: nil)
         
         ariaManager.setup()
+        
+
     }
 
     override var representedObject: AnyObject? {
@@ -45,6 +54,32 @@ class MainViewController: NSViewController {
     
     func globalStatChanged() {
         
+    }
+    
+    @IBAction func saveTo(sender: AnyObject) {
+//        let panel = NSOpenPanel.init(contentRect: NSMakeRect(0, 0, 500, 400), styleMask: 0, backing: .Retained, defer: false)
+//        panel.canChooseFiles = false
+//        panel.canChooseDirectories = true
+//        panel.allowsMultipleSelection = false
+//        panel.beginSheetModalForWindow(self.view.window!) { (result) in
+//            if (result == 1) {
+//                print(panel.URL)
+//            }
+//        }
+    }
+    
+    @IBAction func download(sender: AnyObject) {
+        let urls = self.urlsTextField.stringValue.componentsSeparatedByString(",")
+        ariaManager.client.addUri(urls) { (result) in
+            switch result {
+            case let .Error(error):
+                print(error)
+                break
+            case let .Success(value):
+                print(value)
+                break
+            }
+        }
     }
 }
 
